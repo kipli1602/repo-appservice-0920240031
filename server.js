8c57eaefeffae2c00c9b3b3bc3f2c8b2396ad600
Rafliansyah <cpt.zero04@gmail.com>
2026-05-30 22:52:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Praktikum Azure App Service</h1><p>Aplikasi 1 berjalan.</p>');
+  res.send('<h1>Praktikum Azure App Service</h1><p>Aplikasi berjalan.</p>');
 });
 
 app.get('/status', (req, res) => {
