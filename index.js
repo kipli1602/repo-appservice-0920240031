9d48d36c18787d6b18d2c3b34dedbd7673fcda28
Rafliansyah <cpt.zero04@gmail.com>
2026-05-30 21:27:16 +0700

    4

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Endpoint utama
+// Endpoint utama dengan navigasi
 app.get('/', (req, res) => {
-  res.send('<h1>Praktikum Azure App Service</h1><p>Aplikasi 1 berjalan.</p>');
+  res.send(`
+    <h1>Praktikum Azure App Service</h1>
+    <p>Aplikasi 1 berjalan.</p>
+    <ul>
+      <li><a href="/status">Cek Status</a></li>
+      <li><a href="/profil">Cek Profil</a></li>
+    </ul>
+  `);
 });
 
 // Endpoint status
